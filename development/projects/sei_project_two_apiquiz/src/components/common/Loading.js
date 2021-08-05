0a01344c812c95ba6e12a5b155837142f30f4df2
Harry Evans <henryjohnevans@icloud.com>
2021-08-05 09:43:24 +0100

    file structure made. wireframe components created in js files. some basic code put into api file and loading page brought across from previous project

---
+++ development/projects/sei_project_two_apiquiz/src/components/common/Loading.js
@@ -0,0 +1,18 @@
+function Loading() {
+  
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-6 is-offset-3">
+            <div className="loader-wrapper is-active">
+              <div className="loader is-loading"></div>
+            </div> 
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
---
+++ development/projects/sei_project_two_apiquiz/src/lib/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const baseUrl = 'https://trivia.willfry.co.uk/api/questions?categories='
+
+export function getMovieQuestions() {
+  return axios.get(`${baseUrl}/movies&limit=10`)
+}
